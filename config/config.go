2fc2b41fad5b7777f5fc150e9b5e7a6863d9177a
0f924 <1772407096@qq.com>
2020-03-01 16:09:52 +0800

    实战：配置文件读取

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"log"
+	"strings"
+)
+
+type Config struct {
+	Name string
+}
+
+func Init(cfg string) error {
+	c := Config{
+		Name: cfg,
+	}
+
+	// 初始化配置文件
+	if err := c.initConfig(); err != nil {
+		return err
+	}
+
+	// 监控配置文件变化并热加载程序
+	c.watchConfig()
+
+	return nil
+}
+
+func (c *Config) initConfig() error {
+	if c.Name != "" {
+		viper.SetConfigFile(c.Name)
+	} else {
+		viper.AddConfigPath("conf")
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()            // 读取匹配的环境变量
+	viper.SetEnvPrefix("APISERVER") // 读取环境变量的前缀为 APISERVER
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	if err := viper.ReadInConfig(); err != nil { // viper解析配置文件
+		return err
+	}
+
+	return nil
+}
+
+// 监控配置文件变化并热加载程序
+func (c *Config) watchConfig() {
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Printf("Config file changed: %s", e.Name)
+	})
+}
